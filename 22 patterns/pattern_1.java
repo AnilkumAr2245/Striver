eaec612e5972ee58451200760cba827f26e4a1c9
AnilkumAr2245 <tanneeruanilkumar02@gmail.com>
2026-03-14 09:59:44 +0530

    Implement 4*4 star pattern

--- 22 patterns/pattern_1.java
+++ 22 patterns/pattern_1.java
@@ -6,14 +6,22 @@
  */
 
 
+import java.util.Scanner;
 
 public class pattern_1 {
     public static void main(String[]args){
-        for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
-                System.out.print("* ");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no of rows: ");
+        int rows=sc.nextInt();
+        System.out.print("Enter no of columns: ");
+        int columns=sc.nextInt();
+        System.out.print("Enter any special character: ");
+        String sp=sc.next();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+                System.out.print(sp+"");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
